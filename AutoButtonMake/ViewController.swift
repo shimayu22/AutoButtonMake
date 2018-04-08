3c70b85d3c2ec10c9623c2e77ecf8c9702a933de
Yutaka Shimada <shimayu22@gmail.com>
2018-04-08 15:37:05 +0900

    forで複数のボタンを作成

--- AutoButtonMake/ViewController.swift
+++ AutoButtonMake/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 1 ... 3{
+            //ボタンの初期化
+            var simpleButton = UIButton()
+
+            //表示する文字
+            simpleButton.setTitle(String(i), for: .normal)
+            
+            //ボタンの文字色
+            simpleButton.setTitleColor(UIColor.blue, for: .normal)
+            
+            //ボタンの色
+            simpleButton.backgroundColor = UIColor.orange
+            
+            //タップした時の文字色
+            simpleButton.setTitleColor(UIColor.white, for: .highlighted)
+            
+            //サイズ
+            simpleButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            
+            //配置場所
+            simpleButton.layer.position = CGPoint(x: i*60, y: 100)
+            
+            //tagの番号
+            simpleButton.tag = i
+            
+            self.view.addSubview(simpleButton)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
