87ecfb3729616d13fd9f925ad756fc5697f9a278
Yutaka Shimada <shimayu22@gmail.com>
2018-04-08 11:26:01 +0900

    Initial commit

---
+++ AutoButtonMake/MakeButton.swift
@@ -0,0 +1,15 @@
+//
+//  MakeButton.swift
+//  AutoButtonMake
+//
+//  Created by 島田裕 on 2018/04/08.
+//  Copyright © 2018年 shimayu. All rights reserved.
+//
+
+import UIKit
+
+class MakeButton: NSObject {
+    
+    
+
+}
